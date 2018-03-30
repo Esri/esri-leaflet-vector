4ce81c7f1301c26e40b66b19d511834eec3430c9
john gravois <jgravois@esri.com>
2018-03-29 22:27:50 -0700

    bump to v2 hosted vector tile sources

--- src/Basemap.js
+++ src/Basemap.js
@@ -7,19 +7,24 @@ export var Basemap = L.Layer.extend({
     URLPREFIX: 'https://www.arcgis.com/sharing/rest/content/items/',
     URLSUFFIX: '/resources/styles/root.json',
     STYLES: {
-      'DarkGray': '57436c01bc754dbb87dfb636b6484022',
-      'Gray': '1e47168d181248e491541ffd5a91c0de',
-      'Hybrid': 'af6063d6906c4eb589dfe03819610660',
-      'Navigation': 'e19e9330bf08490ca8353d76b5e2e658',
-      'Streets': 'a60a37a27cc140ddad15f919cd5a69f2',
-      'StreetsNight': '92c551c9f07b4147846aae273e822714',
-      'StreetsRelief': '78c0a9ab4fbf4198a8b951848aab19d8',
-      'Topographic': '86d5ed4b6dc741de9dad5f0fbe09ae95',
+      'DarkGray': '5e9b3685f4c24d8781073dd928ebda50',
+      'DarkGrayLabels' : '747cb7a5329c478cbe6981076cc879c5',
+      'Gray': '291da5eab3a0412593b66d384379f89f',
+      'Hybrid': '30d6b8271e1849cd9c3042060001f425',
+      'Navigation': '63c47b7177f946b49902c24129b87252',
+      'Streets': 'de26a3cf4cc9451298ea173c4b324736',
+      'StreetsNight': '86f556a2d1fd468181855a35e344567f',
+      'StreetsRelief': '41597245552743d5910de614d47e748c',
+      'Topographic': '7dc6cea0b1764a1f9af2e679f642f0f5',
       'Spring': '267f44f08a844c7abee2b62b00600540',
-      'Newspaper': '4f4843d99c34436f82920932317893ae',
-      'MidCentury': '763884983d3544c0a418a97992881fce',
-      'ModernAntique': '996d9e7a3aac4514bb692ce7a990f1c1',
-      'BlackAndWhite': '3161443179244702a5e0449010013b54'
+      'Newspaper': 'dfb04de5f3144a80bc3f9f336228d24a',
+      'MidCentury': '7675d44bb1e4428aa2c30a9b68f97822',
+      'ModernAntique': 'effe3475f05a4d608e66fd6eeb2113c0',
+      'BlackAndWhite': '3161443179244702a5e0449010013b54',
+      'ColoredPencil': '4cf7e1fb9f254dcda9c8fbadb15cf0f8',
+      'HumanGeography': '97fa1365da1e43eabb90d0364326bc2d',
+      'DarkHumanGeography': 'd7397603e9274052808839b70812be50',
+      'Nova': '75f4dfdff19e445395653121a95a85db'
     }
   },
 
@@ -39,7 +44,6 @@ export var Basemap = L.Layer.extend({
 
   onAdd: function (map) {
     this._map = map;
-
     Util.setEsriAttribution(map);
 
     if (map.attributionControl) {
@@ -71,11 +75,20 @@ export var Basemap = L.Layer.extend({
   _asyncAdd: function () {
     var map = this._map;
 
+    // thought it was just me, but apparently its not easy to mixin two different styles
+    // https://github.com/mapbox/mapbox-gl-js/issues/4000
+    // https://www.mapbox.com/mapbox-gl-js/style-spec/#sources
+    // if (map._gl) {
+      // map._gl._glMap.addSource("second", this._mapboxGL.options.style.sources.esri);
+    // }
+    
     // set the background color of the map to the background color of the tiles
     map.getContainer().style.background = this._mapboxGL.options.style.layers[0].paint['background-color'];
 
     map.on('moveend', Util._updateMapAttribution);
     this._mapboxGL.addTo(map, this);
+    // map._gl = this._mapboxGL;
+    
   }
 });
 
