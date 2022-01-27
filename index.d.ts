f3eb5b4871835c920403f7f421f82ddc949cd28b
Gavin Rehkemper <gavinr@users.noreply.github.com>
2022-01-27 13:56:15 -0800

    added basic typescript types file (#114)

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module 'esri-leaflet-vector' {
+  export function vectorBasemapLayer(key: any, options: any): any;
+  export var VectorBasemapLayer: any;
+  export function vectorTileLayer(key: any, options: any): any;
+  export var VectorTileLayer: any;
+  export function mapboxGLJSLayer(options: any): any;
+  export var MapboxGLJSLayer: any;
+}
