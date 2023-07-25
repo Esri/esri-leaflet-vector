356849e3734816610bec8cb2b026bf033f10237b
Tim Stackhouse <tstackhouse@gmail.com>
2023-07-25 16:09:57 -0400

    update export of package version

--- src/EsriLeafletVector.js
+++ src/EsriLeafletVector.js
@@ -1,3 +1,6 @@
-export { version as VERSION } from '../package.json';
+import packageInfo from '../package.json';
+
+export const VERSION = packageInfo.version;
+
 export { VectorBasemapLayer, vectorBasemapLayer } from './VectorBasemapLayer';
 export { VectorTileLayer, vectorTileLayer } from './VectorTileLayer';
