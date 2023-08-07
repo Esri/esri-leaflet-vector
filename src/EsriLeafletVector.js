bbc8d12707c40530cc68469d2f258e2f2e2dfa4e
Tim Stackhouse <tim@tstackhouse.com>
2023-08-07 15:14:49 -0400

    update export of package version (#187)
    * update export of package version
    
    ---------
    
    Co-authored-by: Gavin Rehkemper <grehkemper@esri.com>

--- src/EsriLeafletVector.js
+++ src/EsriLeafletVector.js
@@ -1,3 +1,7 @@
-export { version as VERSION } from '../package.json';
+// export version
+import packageInfo from '../package.json';
+const version = packageInfo.version;
+export { version as VERSION };
+
 export { VectorBasemapLayer, vectorBasemapLayer } from './VectorBasemapLayer';
 export { VectorTileLayer, vectorTileLayer } from './VectorTileLayer';
