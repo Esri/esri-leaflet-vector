ba913e71571d193144e3e3d075f945afd34c98a2
Gavin Rehkemper <grehkemper@esri.com>
2023-08-07 12:20:29 -0500

    export Util.formatStyle
    so we can run unit tests

--- src/EsriLeafletVector.js
+++ src/EsriLeafletVector.js
@@ -1,3 +1,4 @@
 export { version as VERSION } from '../package.json';
 export { VectorBasemapLayer, vectorBasemapLayer } from './VectorBasemapLayer';
 export { VectorTileLayer, vectorTileLayer } from './VectorTileLayer';
+export { EsriUtil as Util } from './Util';
