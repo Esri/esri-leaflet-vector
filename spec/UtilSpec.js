2d5baf7355c406b5f009db662d7b790c4480a03c
Gavin Rehkemper <grehkemper@esri.com>
2023-08-07 12:42:49 -0500

    unit test to check if token added to glyphs

--- spec/UtilSpec.js
+++ spec/UtilSpec.js
@@ -78,4 +78,67 @@ describe('Util', function () {
 
     expect(style.sprite).to.equal(`${spriteUrl}?token=${token}`);
   });
+
+  it('should include the token in the glyph URL when the the URL does not start with http', function () {
+    const token = 'asdf';
+    const styleUrl =
+      'https://tiles.arcgis.com/tiles/test/arcgis/rest/services/test/VectorTileServer/resources/styles/root.json';
+    const fullGlyphUrl =
+      'https://tiles.arcgis.com/tiles/test/arcgis/rest/services/test/VectorTileServer/resources/fonts/{fontstack}/{range}.pbf';
+
+    const style = L.esri.Vector.Util.formatStyle(
+      {
+        version: 8,
+        sprite: 'https://www.arcgis.com/sharing/rest/content/items/123456789/resources/sprites/sprite-1679474043120',
+        glyphs: '../fonts/{fontstack}/{range}.pbf',
+        sources: {
+          esri: {
+            type: 'vector',
+            attribution: 'test',
+            bounds: [-180, -85.0511, 180, 85.0511],
+            minzoom: 0,
+            maxzoom: 19,
+            scheme: 'xyz',
+            url: '../../'
+          }
+        },
+        layers: []
+      },
+      styleUrl,
+      metadata,
+      token
+    );
+
+    expect(style.glyphs).to.equal(`${fullGlyphUrl}?token=${token}`);
+  });
+
+  it('should include the token in the glyph URL when the the URL does start with http', function () {
+    const token = 'asdf';
+    const glyphUrl = 'https://tiles.arcgis.com/tiles/U2DMZtrcqfn913et/arcgis/rest/services/DynamicConsumerMapWebMercator/VectorTileServer/resources/fonts/{fontstack}/{range}.pbf';
+
+    const style = L.esri.Vector.Util.formatStyle(
+      {
+        version: 8,
+        sprite: 'https://www.arcgis.com/sharing/rest/content/items/123456789/resources/sprites/sprite-1679474043120',
+        glyphs: glyphUrl,
+        sources: {
+          esri: {
+            type: 'vector',
+            attribution: 'test',
+            bounds: [-180, -85.0511, 180, 85.0511],
+            minzoom: 0,
+            maxzoom: 19,
+            scheme: 'xyz',
+            url: '../../'
+          }
+        },
+        layers: []
+      },
+      'nope',
+      metadata,
+      token
+    );
+
+    expect(style.glyphs).to.equal(`${glyphUrl}?token=${token}`);
+  });
 });
